2a525c29862237497c7868d8f923b6056aae306c
Ai Vi Tran <aivitran6704@gmail.com>
2025-10-18 17:48:57 +1100

    bootstrap added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
 import StudentForm from "./components/StudentForm";
-// import AdminDashboard from "./components/AdminDashboard";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import AdminDashboard from "./components/AdminDashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import "./styles/login.css";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/student-form" element={<StudentForm />} />
-          {/* <Route path="/admin" element={<AdminDashboard />} /> */}
+          <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
       </div>
     </Router>
