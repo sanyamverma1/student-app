dc949e8d17029b91ad6ffff4e4d7a85952f07607
Ai Vi Tran <aivitran6704@gmail.com>
2025-10-22 14:32:31 +1100

    admin dashboard accessible

--- backend/index.js
+++ backend/index.js
@@ -124,6 +124,14 @@ app.post("/api/submit", async (req, res) => {
     });
   }
 });
+app.get("/api/submissions", async (req, res) => {
+  try {
+    const students = await Student.find();
+    res.status(200).json(students);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch submissions" });
+  }
+});
 
 app.listen(5000, () => console.log(" Server running on http://localhost:5000"));
 
